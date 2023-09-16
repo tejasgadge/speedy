176312bc7af59b2e770dd58b174068c4c8511d3b
tejasgadge <tejasgadge2882@gmail.com>
2023-09-16 16:02:21 +0530

    Created Basic UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,171 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect,useRef} from "react"
+import {generate} from 'random-words'
+const NUMB_OF_WORDS = 50
+const SECONDS = 60
 function App() {
+
+  let interval;
+  const [countDown, setCountDown] = useState(SECONDS);
+  const [words, setWords] = useState([]);
+  const [currInput, setCurrInput] = useState();
+  const textInput = useRef(null)
+  const [status, setStatus] = useState("waiting")
+
+  const [currWordIndex, setCurrWordIndex] = useState(0)
+  const [currCharIndex, setCurrCharIndex] = useState(-1)
+  const [currChar, setCurrChar] = useState("")
+  const [correct, setCorrect] = useState(0)
+  const [incorrect, setIncorrect] = useState(0)
+
+  useEffect(() => {
+    setWords(generateWords())
+  }, [])
+
+  useEffect(() => {
+    if (status === 'started') {
+      textInput.current.focus()
+    }
+  }, [status])
+
+  function handleKeyDown({keyCode, key}) {
+
+    // space bar hit
+    if (keyCode === 32) {
+      checkMatch()
+      setCurrInput(" ")
+      setCurrWordIndex(currWordIndex + 1)
+      setCurrCharIndex(-1)
+
+    // If  backspace is hit
+    } else if (keyCode === 8) {
+      setCurrCharIndex(currCharIndex - 1)
+      setCurrChar("")
+
+    } else {
+      setCurrCharIndex(currCharIndex + 1)
+      setCurrChar(key)
+    }
+  }
+
+  function generateWords() {
+    return new Array(NUMB_OF_WORDS).fill(null).map(() => generate())
+  }
+
+  function start()
+  {
+    if (status === 'finished') {
+      setWords(generateWords())
+      setCurrWordIndex(0)
+      setCorrect(0)
+      setIncorrect(0)
+      setCurrCharIndex(-1)
+      setCurrChar("")
+    }
+
+    if (status !== 'started') {
+      setStatus('started')
+      interval = setInterval(() => {
+        setCountDown((prevCountdown) => {
+          if (prevCountdown === 0) {
+            clearInterval(interval)
+            setStatus('finished')
+            setCurrInput("")
+            return SECONDS
+          } 
+          // else if(status==='waiting'){
+          //   clearInterval(interval);
+          //   setStatus('waiting');
+          //   setCurrInput('');
+          //   return SECONDS;
+          // } 
+          else {
+            return prevCountdown - 1
+          }
+        }  )
+      } ,  1000 )
+    }
+  }
+
+  function reset()
+  {
+    console.log(interval)
+    clearInterval(interval)
+    setStatus('waiting')
+    setCountDown(SECONDS)
+    setCurrInput("")
+  }
+
+  function checkMatch() {
+    const wordToCompare = words[currWordIndex]
+    const doesItMatch = wordToCompare === currInput.trim()
+    if (doesItMatch) {
+      setCorrect(correct + 1)
+    } else {
+      setIncorrect(incorrect + 1)
+    }
+  }
+
+  function getCharClass(wordIdx, charIdx, char) {
+    if (wordIdx === currWordIndex && charIdx === currCharIndex && currChar && status !== 'finished') {
+      if (char === currChar) {
+        return "bg-green-400"
+      } else {
+        return "bg-red-400"
+      }
+    } else if (wordIdx === currWordIndex && currCharIndex >= words[currWordIndex].length) {
+      return "bg-red-400"
+    } else {
+      return ""
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-full w-full bg-sky-200 ">
+      <div className="p-20 flex justify-around items-center flex-col">
+        <div><span className="text-white  bg-blue-950 rounded-md p-1 text-3xl font-bold w-full float-left">{countDown}</span></div>
+        <div className="w-full bg-slate-700 rounded-md m-4 p-2">
+          {words.map((word, i) => (
+            <span key={i}>
+              <span className="text-2xl text-white leading-10 font-normal">
+                {word.split("").map((char, idx) => (
+                  <span className={getCharClass(i, idx, char)} key={idx}>{char}</span>
+                ))}
+              </span>
+              <span> </span>
+            </span>
+          ))}
+        </div>
+        <div className={`m-5 w-full ${status !== "started" ? "hidden" : "visible"}`}>
+          <input type="text" disabled={status !== "started"} onKeyDown={handleKeyDown}  ref={textInput} className="border-slate-100 rounded-md p-2 w-full text-2xl font-normal font-mono" value={currInput} onChange={(e)=>{setCurrInput(e.target.value)}}>
+          </input>
+        </div>
+        <button className="text-3xl text-center rounded-lg bg-blue-950 text-white p-2 W-md" onClick={status==='started'?reset:start}>
+          {status==="started"?"Reset":"Start"}
+          {console.log("hello")}
+        </button>
+      </div>
+      {console.log("Hello")}
+      {status === 'finished' && (
+          <>
+            <div className="flex justify-center gap-2 text-2xl">
+              <div className="">Words per minute:</div>
+              <div className="">
+                {correct}
+              </div>
+            </div>
+            <div className="flex justify-center gap-2 text-2xl font-mono">
+              <div className="">Accuracy:</div>
+              {correct !== 0 ? (
+                <div className="">
+                  {Math.round((correct / (correct + incorrect)) * 100)}%
+                </div>
+              ) : (
+                <div className="">0%</div>
+                )}
+            </div>
+          </>
+      )}
+
     </div>
   );
 }
